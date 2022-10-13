265c224ed15510db9086250fdb5d85d467915832
JY <pianowell96@gmail.com>
2022-10-14 04:17:22 +0900

    [ feat ] modal 구현

--- week1&week2/velog/src/main.js
+++ week1&week2/velog/src/main.js
@@ -1,11 +1,18 @@
 const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
 
 const dropdownBox = $(".dropdown");
 const dropdownBtn = $(".week-btn");
 const dropdownText = $(".week-text");
 
+const body = $("body");
+const root = $(".root");
+const cardBox = $(".contents-container");
+const card = $$(".section__content-card");
+
 let isDropdownOpen = false;
 
+//DropDown
 function handleDropdown() {
   if (isDropdownOpen === false) {
     dropdownBox.classList.remove("hidden");
@@ -30,4 +37,41 @@ function changeDropdownStyle(element) {
 dropdownBtn.addEventListener("click", handleDropdown);
 dropdownBox.addEventListener("click", (e) => {
   changeView(e);
+});
+
+//Modal
+function createModal(selectedContent) {
+  const modalBg = document.createElement("div");
+  const modal = selectedContent.cloneNode(true);
+
+  root.appendChild(modalBg);
+  modalBg.appendChild(modal);
+
+  modal.classList.add("modal");
+  modalBg.classList.add("modal-bg");
+
+  body.style.overflow = "hidden";
+
+  createCloseBtn(modalBg);
+}
+
+function createCloseBtn(parrentElement) {
+  const closeBtn = document.createElement("button");
+  parrentElement.appendChild(closeBtn);
+  closeBtn.classList.add("closeBtn");
+
+  closeModal(closeBtn);
+}
+
+function closeModal(element) {
+  const modalBox = $(".modal-bg");
+  element.addEventListener("click", () => {
+    modalBox.remove();
+  });
+}
+
+card.forEach((item) => {
+  item.addEventListener("click", () => {
+    createModal(item);
+  });
 });
