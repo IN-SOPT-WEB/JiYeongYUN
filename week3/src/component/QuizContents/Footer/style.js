061444713924c637cb5f6342e9cd3f36bc7860c7
NaveOWO <pianowell96@gmail.com>
2022-10-29 18:15:05 +0900

    [ design ] footer 스타일

---
+++ week3/src/component/QuizContents/Footer/style.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const St = {
+  ButtonContainer: styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `,
+
+  RetryButton: styled.button`
+    background-color: aqua;
+
+    height: 3rem;
+    width: 10rem;
+  `,
+};
