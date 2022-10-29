76fcd6316c1f2952c57b8b6831c04add3b0a089d
NaveOWO <pianowell96@gmail.com>
2022-10-29 18:14:33 +0900

    [ disign ] score 컴포넌트 스타일

---
+++ week3/src/component/QuizContents/MyScore/style.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const St = {
+  ScoreContainer: styled.section`
+    background-color: pink;
+
+    height: 3rem;
+    width: 100%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `,
+
+  ScoreText: styled.h2`
+    color: white;
+
+    font-size: 1.5rem;
+  `,
+};
