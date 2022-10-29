474927c8a97d7a6cef494875df57e52185bd3e7f
NaveOWO <pianowell96@gmail.com>
2022-10-29 18:16:03 +0900

    [ feat ] header 컴포넌트 구현

---
+++ week3/src/component/Header/index.jsx
@@ -0,0 +1,9 @@
+import { St } from "./style";
+
+export default function Header() {
+  return (
+    <St.HeaderContainer>
+      <St.HeaderText>주함이는 지금?</St.HeaderText>
+    </St.HeaderContainer>
+  );
+}
