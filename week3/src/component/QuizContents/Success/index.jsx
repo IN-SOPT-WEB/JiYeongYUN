689b2282d381ccff641153bcc3082445883d7332
NaveOWO <pianowell96@gmail.com>
2022-10-29 18:12:14 +0900

    [ feat ] 완료 컴호넌트 구현

---
+++ week3/src/component/QuizContents/Success/index.jsx
@@ -0,0 +1,6 @@
+import { St } from "./style";
+import successImg from "../../../images/success.jpeg";
+
+export default function Success() {
+  return <St.SuccessImg src={successImg}></St.SuccessImg>;
+}
