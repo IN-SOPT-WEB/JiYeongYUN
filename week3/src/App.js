0146b01dae03ce70002c125de782b0bd1f47c397
NaveOWO <pianowell96@gmail.com>
2022-11-04 03:12:55 +0900

    [ feat ] 컴포넌트 병합

--- week3/src/App.js
+++ week3/src/App.js
@@ -1,23 +1,14 @@
-import styled from "styled-components";
 import Header from "./component/Header";
+import Modal from "./component/Modal";
 import QuizContents from "./component/QuizContents";
-import snoopy from "../src/images/snoopy.png";
 
 function App() {
   return (
-    <Root>
+    <>
       <Header />
       <QuizContents />
-    </Root>
+    </>
   );
 }
-
-const Root = styled.body`
-  background-color: lightcoral;
-  background-image: url(${snoopy});
-  background-size: 15%;
-
-  height: 100vh;
-`;
 
 export default App;
