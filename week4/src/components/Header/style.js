b56f0ed1a4475546b1206fb42b4447346f6c1fe9
NaveOWO <pianowell96@gmail.com>
2022-11-10 23:22:11 +0900

    [ design ] header component 스타일링

---
+++ week4/src/components/Header/style.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const TitleContainer = styled.section`
+  height: 10rem;
+
+  display: flex;
+  flex-direction: column-reverse;
+  align-items: center;
+`;
+
+const TitleText = styled.h1`
+  margin: 0;
+  font-size: 4rem;
+  color: white;
+`;
+
+const St = {
+  TitleContainer,
+  TitleText,
+};
+
+export default St;
