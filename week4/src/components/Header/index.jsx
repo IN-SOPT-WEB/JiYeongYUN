80cfbdd72ef52b283d49161f209ada629c0aa882
NaveOWO <pianowell96@gmail.com>
2022-11-10 23:21:55 +0900

    [ feat ] header component 구현

---
+++ week4/src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+import St from "./style";
+
+export default function Header() {
+  return (
+    <St.TitleContainer>
+      <St.TitleText>Git Finder</St.TitleText>
+    </St.TitleContainer>
+  );
+}
