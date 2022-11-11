aa7b251ccc3698c2b3308241fad0982b50c8aab5
NaveOWO <pianowell96@gmail.com>
2022-11-11 21:18:46 +0900

    [ feat ] api constants 생성

---
+++ week4/src/core/api/constants.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+export const TOKEN = "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";
+
+const instance = axios.create({
+  baseURL: "https://api.github.com/users",
+  Authorization: "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",
+  headers: {
+    Authorization: `Bearer ${TOKEN}`,
+  },
+});
+
+export async function GET_DATA(path) {
+  try {
+    const result = await instance.get(path);
+    return result.data;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export function getUserInfo(userName) {
+  return GET_DATA(userName);
+}
+export function getUserStar(userName) {
+  return GET_DATA(`${userName}/starred`);
+}
