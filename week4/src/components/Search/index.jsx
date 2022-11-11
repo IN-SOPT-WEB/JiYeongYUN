b84ab5c112167dfd2c36d0f0263c90007446d93b
NaveOWO <pianowell96@gmail.com>
2022-11-11 21:20:26 +0900

    [ feat ] history box 구현

--- week4/src/components/Search/index.jsx
+++ week4/src/components/Search/index.jsx
@@ -1,12 +1,39 @@
 import St from "./style";
 import searchIcon from "../../images/loupe.png";
-export default function Contents() {
+import { useState } from "react";
+
+export default function Search(props) {
+  const {
+    getData,
+    searchHistory,
+    getHistoryData,
+    isFocused,
+    changeFocus,
+    historyTarget,
+  } = props;
+
   return (
     <St.SearchBarContainer>
       <St.SearchBar>
         <St.SearchIcon src={searchIcon}></St.SearchIcon>
-        <St.SearchInput placeholder="who?"></St.SearchInput>
+        <St.SearchInput
+          placeholder="Github Name"
+          onKeyDown={getData}
+          onClick={changeFocus}
+          ref={historyTarget}></St.SearchInput>
       </St.SearchBar>
+      {isFocused && (
+        <St.HistoryBox>
+          {[...searchHistory].map((item, index) => (
+            <St.HistoryList
+              ref={historyTarget}
+              onClick={getHistoryData}
+              key={index}>
+              {item}
+            </St.HistoryList>
+          ))}
+        </St.HistoryBox>
+      )}
     </St.SearchBarContainer>
   );
 }
